8531a0ca142ec1dd350cfc1944b5acc4d1ab41ff
smash <smash@PABLO>
2024-05-22 08:35:00 -0400

    Text Styling Works

--- DesktopOverlayUI/pages/overlayMenu/TextStyleTab.xaml.cs
+++ DesktopOverlayUI/pages/overlayMenu/TextStyleTab.xaml.cs
@@ -25,8 +25,9 @@ namespace DesktopOverlayUI.pages.overlayMenu
 
         public TextStyleTab(OverlayDisplay overlay)
         {
-            InitializeComponent();
             _overlay = overlay;
+            InitializeComponent();
+            FontFamilyComboBox.SelectedItem = new FontFamily("Segoe UI");
         }
 
         private void UpdateColorPreview(object sender, RoutedPropertyChangedEventArgs<double> e)
@@ -38,6 +39,65 @@ namespace DesktopOverlayUI.pages.overlayMenu
             _overlay.OverlayTextBlock.Foreground = new SolidColorBrush(Color.FromRgb((byte)RedSlider.Value, (byte)GreenSlider.Value, (byte)BlueSlider.Value));
         }
 
+        private void UpdateFontSize(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            FontSizeSlider.Value = (int)FontSizeSlider.Value;
+            _overlay.OverlayTextBlock.FontSize = FontSizeSlider.Value;
+        }
+
+        private void UpdateFontFamily(object sender, SelectionChangedEventArgs e)
+        {
+            var fontFamily = (FontFamily)FontFamilyComboBox.SelectedItem;
+            _overlay.OverlayTextBlock.FontFamily = fontFamily;
+        }
+
+        private void HexValueChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!System.Text.RegularExpressions.Regex.IsMatch(HexTextBox.Text, @"\A\b[0-9a-fA-F]+\b\Z"))
+            {
+                HexTextBox.Text = "";
+            }
+            if (HexTextBox.Text.Length != 6) return;
+            var color = ColorConverter.ConvertFromString("#" + HexTextBox.Text);
+            if (color == null) return;
+            var rgb = Color.FromRgb(((Color)color).R, ((Color)color).G, ((Color)color).B);
+            RedSlider.Value = rgb.R;
+            GreenSlider.Value = rgb.G;
+            BlueSlider.Value = rgb.B;
+            ColorPreview.Background = new SolidColorBrush(rgb);
+            _overlay.OverlayTextBlock.Foreground = new SolidColorBrush(rgb);
+        }
+
+        private void RGBValueChanged(object sender, TextChangedEventArgs e)
+        {
+            if (RedTextBox == null || GreenTextBox == null || BlueTextBox == null) return;
+            if (!System.Text.RegularExpressions.Regex.IsMatch(RedTextBox.Text, @"\A\b[0-9]+\b\Z") || int.Parse(RedTextBox.Text) > 255)
+            {
+                RedTextBox.Text = "";
+            }
+
+            if (!System.Text.RegularExpressions.Regex.IsMatch(GreenTextBox.Text, @"\A\b[0-9]+\b\Z") || int.Parse(GreenTextBox.Text) > 255)
+            {
+                GreenTextBox.Text = "";
+            }
+
+            if (!System.Text.RegularExpressions.Regex.IsMatch(BlueTextBox.Text, @"\A\b[0-9]+\b\Z") || int.Parse(BlueTextBox.Text) > 255)
+            {
+                BlueTextBox.Text = "";
+            }
+
+            if (RedTextBox.Text.Length != 0 && GreenTextBox.Text.Length != 0 && BlueTextBox.Text.Length != 0)
+            {
+                var rgb = Color.FromRgb(byte.Parse(RedTextBox.Text), byte.Parse(GreenTextBox.Text), byte.Parse(BlueTextBox.Text));
+                RedSlider.Value = rgb.R;
+                GreenSlider.Value = rgb.G;
+                BlueSlider.Value = rgb.B;
+                ColorPreview.Background = new SolidColorBrush(rgb);
+                string toHex = rgb.R.ToString("X2") + rgb.G.ToString("X2") + rgb.B.ToString("X2");
+                HexTextBox.Text = toHex;
+                _overlay.OverlayTextBlock.Foreground = new SolidColorBrush(rgb);
+            }
+        }
         
         
 
